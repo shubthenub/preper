a6a51e1d72ebcd28a3faae5d41e3bfaaee0f3e18
Shashank Shubham <shashankshubham007@gmail.com>
2025-12-20 03:48:09 +0530

    fix attempt 4 (middleware bloating)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next"
+// import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next"
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 // import { env } from "./data/env/server"
 
@@ -8,28 +8,28 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhooks(.*)",
 ])
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY!,
-  rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
-    }),
-    slidingWindow({
-      mode: "LIVE",
-      interval: "1m",
-      max: 100,
-    }),
-  ],
-})
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY!,
+//   rules: [
+//     shield({ mode: "LIVE" }),
+//     detectBot({
+//       mode: "LIVE",
+//       allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+//     }),
+//     slidingWindow({
+//       mode: "LIVE",
+//       interval: "1m",
+//       max: 100,
+//     }),
+//   ],
+// })
 
 export default clerkMiddleware(async (auth, req) => {
-  const decision = await aj.protect(req)
+  // const decision = await aj.protect(req) temporarily removing arcjet logic as exceeding vercels 1mb limit
 
-  if (decision.isDenied()) {
-    return new Response(null, { status: 403 })
-  }
+  // if (decision.isDenied()) {
+  //   return new Response(null, { status: 403 })
+  // }
 
   if (!isPublicRoute(req)) {
     await auth.protect()
